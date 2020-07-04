9c5608d4c5ff8bc0200997a512f5f73b49843be7
Ali Khaled <alikha1ed@outlook.com>
2020-07-04 17:04:35 +0200

    remove image from products request

--- routers/product.js
+++ routers/product.js
@@ -1,24 +1,23 @@
-const express = require('express')
-const Product = require('../models/product')
-const auth = require('../middleware/auth')
-const multer = require('multer')
-const sharp = require('sharp')
-const router = new express.Router()
+const express = require('express');
+const Product = require('../models/product');
+const auth = require('../middleware/auth');
+const multer = require('multer');
+const sharp = require('sharp');
+const router = new express.Router();
 
 router.post('/products/:id', async (req, res) => {
-    
-    const product = new Product({
-        ...req.body,
-        categoryId: req.params.id
-    })
+	const product = new Product({
+		...req.body,
+		categoryId: req.params.id,
+	});
 
-    try {
-        await product.save()
-        res.status(201).send(product)
-    } catch (e) {
-        res.status(400).send(e)
-    } 
-})
+	try {
+		await product.save();
+		res.status(201).send(product);
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
 /*
 // GET /products?completed=true
 // GET /products?limit=10&skip=20
@@ -52,145 +51,160 @@ router.get('/products', async (req, res) => {
     }
 })*/
 
-
-
 router.get('/products', async (req, res) => {
-   // const _id = req.params.id
+	// const _id = req.params.id
 
-    try {
-        const products = await Product.find();
+	try {
+		const products = await Product.find().select(
+			'name description width height depth color categoryId'
+		);
 
-        if (!products) {
-            return res.status(404).send()
-        }
+		if (!products) {
+			return res.status(404).send();
+		}
 
-        res.send(products)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-
+		res.send(products);
+	} catch (e) {
+		res.status(500).send();
+	}
+});
 
 router.get('/products/:id', async (req, res) => {
-    const _id = req.params.id
+	const _id = req.params.id;
 
-    try {
-        const product = await Product.findOne({ _id })
+	try {
+		const product = await Product.findOne({ _id });
 
-        if (!product) {
-            return res.status(404).send()
-        }
+		if (!product) {
+			return res.status(404).send();
+		}
 
-        res.send(product)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+		res.send(product);
+	} catch (e) {
+		res.status(500).send();
+	}
+});
 
 //Get Products in the Category
 
 router.get('/productscat/:id', async (req, res) => {
-    const _id = req.params.id
-    console.log(req.params.id)
+	const _id = req.params.id;
+	console.log(req.params.id);
 
-    try {
-        
-        const products = await Product.find({ categoryId: req.params.id })
+	try {
+		const products = await Product.find({ categoryId: req.params.id });
 
-        if (!products) {
-            return res.status(404).send()
-        }
+		if (!products) {
+			return res.status(404).send();
+		}
 
-        res.send(products)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
+		res.send(products);
+	} catch (e) {
+		res.status(500).send();
+	}
+});
 
 router.patch('/products/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'description', 'price', 'category', 'image', 'color', 'width', 'height', 'depth']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+	const updates = Object.keys(req.body);
+	const allowedUpdates = [
+		'name',
+		'description',
+		'price',
+		'category',
+		'image',
+		'color',
+		'width',
+		'height',
+		'depth',
+	];
+	const isValidOperation = updates.every(update =>
+		allowedUpdates.includes(update)
+	);
 
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
-    }
+	if (!isValidOperation) {
+		return res.status(400).send({ error: 'Invalid updates!' });
+	}
 
-    try {
-        const product = await Product.findOne({ _id: req.params.id})
+	try {
+		const product = await Product.findOne({ _id: req.params.id });
 
-        if (!product) {
-            return res.status(404).send()
-        }
+		if (!product) {
+			return res.status(404).send();
+		}
 
-        updates.forEach((update) => product[update] = req.body[update])
-        await product.save()
-        res.send(product)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
+		updates.forEach(update => (product[update] = req.body[update]));
+		await product.save();
+		res.send(product);
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
 
 router.delete('/products/:id', async (req, res) => {
-    try {
-        const product = await Product.findOneAndDelete({ _id: req.params.id })
+	try {
+		const product = await Product.findOneAndDelete({ _id: req.params.id });
 
-        if (!product) {
-            res.status(404).send()
-        }
+		if (!product) {
+			res.status(404).send();
+		}
 
-        res.send(product)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
+		res.send(product);
+	} catch (e) {
+		res.status(500).send();
+	}
+});
 
 const upload = multer({
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-            return cb(new Error('Please upload an image'))
-        }
+	limits: {
+		fileSize: 1000000,
+	},
+	fileFilter(req, file, cb) {
+		if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+			return cb(new Error('Please upload an image'));
+		}
 
-        cb(undefined, true)
-    }
-})
+		cb(undefined, true);
+	},
+});
 
-router.post('/products/:id/image', upload.single('image'), async (req, res) => {
-    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
-    const product = await Product.findOne({ _id: req.params.id })
-    product.image = buffer
-    await product.save()
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
+router.post(
+	'/products/:id/image',
+	upload.single('image'),
+	async (req, res) => {
+		const buffer = await sharp(req.file.buffer)
+			.resize({ width: 250, height: 250 })
+			.png()
+			.toBuffer();
+		const product = await Product.findOne({ _id: req.params.id });
+		product.image = buffer;
+		await product.save();
+		res.send();
+	},
+	(error, req, res, next) => {
+		res.status(400).send({ error: error.message });
+	}
+);
 
 router.delete('/products/:id/image', async (req, res) => {
-    const product = await Product.findOne({ _id: req.params.id })
-    product.image = undefined
-    await product.save()
-    res.send()
-})
+	const product = await Product.findOne({ _id: req.params.id });
+	product.image = undefined;
+	await product.save();
+	res.send();
+});
 
 router.get('/products/:id/image', async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id)
+	try {
+		const product = await Product.findById(req.params.id);
 
-        if (!product || !product.image) {
-            throw new Error()
-        }
+		if (!product || !product.image) {
+			throw new Error();
+		}
 
-        res.set('Content-Type', 'image/png')
-        res.send(product.image)
-    } catch (e) {
-        res.status(404).send()
-    }
-})
+		res.set('Content-Type', 'image/png');
+		res.send(product.image);
+	} catch (e) {
+		res.status(404).send();
+	}
+});
 
-module.exports = router
+module.exports = router;
