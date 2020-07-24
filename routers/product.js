4577c1c1a943206be9597fab87e5f1cf628933c2
Ali Khaled <alikha1ed@outlook.com>
2020-07-25 01:18:07 +0200

    enhance the api

--- routers/product.js
+++ routers/product.js
@@ -40,6 +40,12 @@ router.get('/products', async (req, res) => {
 
 router.get('/', async (req, res) => {
 	try {
+		const obj2gltf = require('obj2gltf');
+		const fs = require('fs');
+		obj2gltf('Wooden Chair.obj').then(function (gltf) {
+			const data = Buffer.from(JSON.stringify(gltf));
+			fs.writeFileSync('model.gltf', data);
+		});
 		return res.status(200).json('Welcome to HomeFit API');
 	} catch (e) {
 		return res.status(500).json(e);
@@ -253,9 +259,71 @@ router.get('/products/:id/model', async (req, res) => {
 		var file = fs.createReadStream(`./uploads/${product._id}/scene.gltf`);
 		var stat = fs.statSync(`./uploads/${product._id}/scene.gltf`);
 		res.setHeader('Content-Length', stat.size);
-		res.setHeader('Content-Type', 'model/gltf+json');
-		res.setHeader('Content-Disposition', 'attachment; filename=scene.gltf');
+		//res.setHeader('Content-Type', 'model/gltf+json');
+		res.setHeader('Content-Disposition', 'inline; filename=scene.gltf');
+		//res.setHeader('Content-Encoding', 'gzip');
+		//res.setHeader('X-Content-Type-Options', 'nosniff');
+		//res.setHeader('X-Frame-Options', 'deny');
+		//const url = `https://${req.headers.host}/uploads/${product._id}/scene.gltf`;
+		//const { data } = await Axios.get(url);
+		file.pipe(res);
+		//res.redirect(url);
+	} catch (e) {
+		res.status(404).send({ error: e });
+	}
+});
 
+router.get('/products/:id/:modelFile', async (req, res) => {
+	try {
+		const product = await Product.findById(req.params.id).select('_id');
+		if (!product) {
+			res.status(404).send({ error: 'Product not found!' });
+		}
+		var file = fs.createReadStream(
+			`./uploads/${product._id}/${req.params.modelFile}`
+		);
+		var stat = fs.statSync(
+			`./uploads/${product._id}/${req.params.modelFile}`
+		);
+		res.setHeader('Content-Length', stat.size);
+		//res.setHeader('Content-Type', 'model/gltf+json');
+		res.setHeader(
+			'Content-Disposition',
+			`inline; filename=${req.params.modelFile}`
+		);
+		//res.setHeader('Content-Encoding', 'gzip');
+		//res.setHeader('X-Content-Type-Options', 'nosniff');
+		//res.setHeader('X-Frame-Options', 'deny');
+		//const url = `https://${req.headers.host}/uploads/${product._id}/scene.gltf`;
+		//const { data } = await Axios.get(url);
+		file.pipe(res);
+		//res.redirect(url);
+	} catch (e) {
+		res.status(404).send({ error: e });
+	}
+});
+
+router.get('/products/:id/textures/:texture', async (req, res) => {
+	try {
+		const product = await Product.findById(req.params.id).select('_id');
+		if (!product) {
+			res.status(404).send({ error: 'Product not found!' });
+		}
+		var file = fs.createReadStream(
+			`./uploads/${product._id}/textures/${req.params.texture}`
+		);
+		var stat = fs.statSync(
+			`./uploads/${product._id}/textures/${req.params.texture}`
+		);
+		res.setHeader('Content-Length', stat.size);
+		//res.setHeader('Content-Type', 'model/gltf+json');
+		res.setHeader(
+			'Content-Disposition',
+			`inline; filename=${req.params.texture}`
+		);
+		//res.setHeader('Content-Encoding', 'gzip');
+		//res.setHeader('X-Content-Type-Options', 'nosniff');
+		//res.setHeader('X-Frame-Options', 'deny');
 		//const url = `https://${req.headers.host}/uploads/${product._id}/scene.gltf`;
 		//const { data } = await Axios.get(url);
 		file.pipe(res);
--- routers/user.js
+++ routers/user.js
@@ -1,64 +1,66 @@
-const express = require('express')
-const multer = require('multer')
-const sharp = require('sharp')
-const User = require('../models/user')
-const auth = require('../middleware/auth')
-const { sendWelcomeEmail, sendCancelationEmail } = require('../email/account')
-const router = new express.Router()
+const express = require('express');
+const multer = require('multer');
+const sharp = require('sharp');
+const User = require('../models/user');
+const auth = require('../middleware/auth');
+const { sendWelcomeEmail, sendCancelationEmail } = require('../email/account');
+const router = new express.Router();
 
 router.post('/users', async (req, res) => {
-    const user = new User(req.body)
+	const user = new User(req.body);
 
-    try {
-        await user.save()
-        sendWelcomeEmail(user.email, user.name)
-        const token = await user.generateAuthToken()
-        res.status(201).send({ user, token })
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
+	try {
+		await user.save();
+		sendWelcomeEmail(user.email, user.name);
+		const token = await user.generateAuthToken();
+		res.status(201).send({ user, token });
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
 
 router.post('/users/login', async (req, res) => {
-    try {
-        const user = await User.findByCredentials(req.body.email, req.body.password)
-        const token = await user.generateAuthToken()
-        console.log(req.body)
-        res.send({ user, token })
-    } catch (e) {
-        res.status(400).send(e.message)
-    }
-})
+	try {
+		const user = await User.findByCredentials(
+			req.body.email,
+			req.body.password
+		);
+		const token = await user.generateAuthToken();
+		res.send({ user, token });
+	} catch (e) {
+		res.status(400).send(e.message);
+	}
+});
 
 router.post('/users/logout', auth, async (req, res) => {
-    try {
-        req.user.tokens = req.user.tokens.filter((token) => {
-            return token.token !== req.token
-        })
-        await req.user.save()
+	try {
+		req.user.tokens = req.user.tokens.filter(token => {
+			return token.token !== req.token;
+		});
+		await req.user.save();
 
-        res.send()
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+		res.send();
+	} catch (e) {
+		res.status(500).send();
+	}
+});
 
 router.post('/users/logoutAll', auth, async (req, res) => {
-    try {
-        req.user.tokens = []
-        await req.user.save()
-        res.send()
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+	try {
+		req.user.tokens = [];
+		await req.user.save();
+		res.send();
+	} catch (e) {
+		res.status(500).send();
+	}
+});
 
 router.get('/users/me', auth, async (req, res) => {
-    res.send(req.user)
-})
+	res.send(req.user);
+});
 
 router.get('/users', async (req, res) => {
-    try {
+	try {
 		const users = await User.find();
 
 		if (!users) {
@@ -69,148 +71,155 @@ router.get('/users', async (req, res) => {
 	} catch (e) {
 		res.status(500).send();
 	}
-})
+});
 
 router.get('/totalusers', async (req, res) => {
 	try {
-		await User.countDocuments({}, function(err, count) {
+		await User.countDocuments({}, function (err, count) {
 			if (!count) {
 				return res.status(404).send(err);
+			} else {
+				res.json(count);
 			}
-			else {
-				res.json(count)
-			}
-		})
+		});
+	} catch (e) {
+		res.status(500).send(e);
 	}
-	catch (e) {
-		res.status(500).send(e)
-	}
-})
+});
 
- //Switch account Type 
- router.patch('/switchAcc/:id', auth, async (req, res) => { 
-    if(req.user.accType === 'admin') { 
-        const user = await User.findOne({ _id: req.params.id }) 
-        if(user.accType === 'customer'){ 
-            user.accType = 'admin'; 
-        } else { 
-            user.accType = 'customer'; 
-        }
-        try{
-            await user.save()
-            res.send(user)
-        } catch(e) {
-            res.status(400).send(e)
-        } } else {
-            res.status(401).send({ error: 'Please authenticate as Admin.' })
-        }
-    })
+//Switch account Type
+router.patch('/switchAcc/:id', auth, async (req, res) => {
+	if (req.user.accType === 'admin') {
+		const user = await User.findOne({ _id: req.params.id });
+		if (user.accType === 'customer') {
+			user.accType = 'admin';
+		} else {
+			user.accType = 'customer';
+		}
+		try {
+			await user.save();
+			res.send(user);
+		} catch (e) {
+			res.status(400).send(e);
+		}
+	} else {
+		res.status(401).send({ error: 'Please authenticate as Admin.' });
+	}
+});
 
 //Make Account type Admin
 router.patch('/makeAdmin/:id', auth, async (req, res) => {
-    
-    if(req.user.accType === 'admin'){
-    const user = await User.findOne({ _id: req.params.id })
-    user.accType = 'admin';
-    try {
-        await user.save()
-        res.send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-} else {
-        res.status(401).send({ error: 'Please authenticate as Admin.' })
-    }
-})
+	if (req.user.accType === 'admin') {
+		const user = await User.findOne({ _id: req.params.id });
+		user.accType = 'admin';
+		try {
+			await user.save();
+			res.send(user);
+		} catch (e) {
+			res.status(400).send(e);
+		}
+	} else {
+		res.status(401).send({ error: 'Please authenticate as Admin.' });
+	}
+});
 
 router.patch('/users/me', auth, async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+	const updates = Object.keys(req.body);
+	const allowedUpdates = ['name', 'email', 'password'];
+	const isValidOperation = updates.every(update =>
+		allowedUpdates.includes(update)
+	);
 
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
-    }
+	if (!isValidOperation) {
+		return res.status(400).send({ error: 'Invalid updates!' });
+	}
 
-    try {
-        updates.forEach((update) => req.user[update] = req.body[update])
-        await req.user.save()
-        res.send(req.user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
+	try {
+		updates.forEach(update => (req.user[update] = req.body[update]));
+		await req.user.save();
+		res.send(req.user);
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
 
 // Delete Profile
-router.delete('/deleteuser/:id', auth, async  (req, res) => {
-    if(req.user.accType === 'admin') {
-        await User.deleteOne({ _id: req.params.id })
-        .exec()
-		.then(result => {
-			return res.status(200).json({ message: 'User deleted' });
-		})
-		.catch(err => {
-			return res.status(500).json({ error: err });
-		});
-    }
-    else {
-        res.status(401).send({ error: 'Please authenticate as an admin.' })
-    }
-})
-
+router.delete('/deleteuser/:id', auth, async (req, res) => {
+	if (req.user.accType === 'admin') {
+		await User.deleteOne({ _id: req.params.id })
+			.exec()
+			.then(result => {
+				return res.status(200).json({ message: 'User deleted' });
+			})
+			.catch(err => {
+				return res.status(500).json({ error: err });
+			});
+	} else {
+		res.status(401).send({ error: 'Please authenticate as an admin.' });
+	}
+});
 
 // Delete my own profile
 router.delete('/users/me', auth, async (req, res) => {
-    try {
-        await req.user.remove()
-        sendCancelationEmail(req.user.email, req.user.name)
-        res.send(req.user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+	try {
+		await req.user.remove();
+		sendCancelationEmail(req.user.email, req.user.name);
+		res.send(req.user);
+	} catch (e) {
+		res.status(500).send();
+	}
+});
 
 const upload = multer({
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-            return cb(new Error('Please upload an image'))
-        }
+	limits: {
+		fileSize: 1000000,
+	},
+	fileFilter(req, file, cb) {
+		if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+			return cb(new Error('Please upload an image'));
+		}
 
-        cb(undefined, true)
-    }
-})
+		cb(undefined, true);
+	},
+});
 
-router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
-    req.user.avatar = buffer
-    await req.user.save()
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
+router.post(
+	'/users/me/avatar',
+	auth,
+	upload.single('avatar'),
+	async (req, res) => {
+		const buffer = await sharp(req.file.buffer)
+			.resize({ width: 250, height: 250 })
+			.png()
+			.toBuffer();
+		req.user.avatar = buffer;
+		await req.user.save();
+		res.send();
+	},
+	(error, req, res, next) => {
+		res.status(400).send({ error: error.message });
+	}
+);
 
 router.delete('/users/me/avatar', auth, async (req, res) => {
-    req.user.avatar = undefined
-    await req.user.save()
-    res.send()
-})
+	req.user.avatar = undefined;
+	await req.user.save();
+	res.send();
+});
 
 router.get('/users/:id/avatar', async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id)
+	try {
+		const user = await User.findById(req.params.id);
 
-        if (!user || !user.avatar) {
-            throw new Error()
-        }
+		if (!user || !user.avatar) {
+			throw new Error();
+		}
 
-        res.set('Content-Type', 'image/png')
-        res.send(user.avatar)
-    } catch (e) {
-        res.status(404).send()
-    }
-})
+		res.set('Content-Type', 'image/png');
+		res.send(user.avatar);
+	} catch (e) {
+		res.status(404).send();
+	}
+});
 
-module.exports = router
+module.exports = router;
